56c9e5cecf9b64cdedcc7026e65eabad3594fff2
dev_os <dred.tam@gmail.com>
2021-04-15 22:59:02 +0600

    Added all files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>To-Do list</h1>
+    <ul>
+      <li>
+        <to-do-item> </to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    ToDoItem,
+  },
+
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input  id="todo-item" type="checkbox"/>
+    <label for="todo-item">My Todo Item</label>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
